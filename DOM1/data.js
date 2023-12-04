098cbf2d96abe357c0ddd68225d5e2e657c342ea
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-12-04 13:01:49 +0530

    okie

---
+++ DOM1/data.js
@@ -0,0 +1,13 @@
+function updatePage(){
+    let cdate = new Date().toLocaleDateString();
+    document.getElementById('abc').innerHTML = cdate
+    //document.getElementById('abc').innerHTML = "GM"
+}
+/* function updatePage(){
+    //document.write("GM")
+    console.log("Test Case 123")
+    let ref=document.getElementById('abc')
+    console.log(ref)
+    ref.innerHTML = "Hello,GM"
+}
+ */
---
+++ DOM2/data.js
@@ -0,0 +1,9 @@
+function change_Color(){
+    /* alert("Test Case123")
+    console.log("Test Case 123") */
+    document.getElementById('abc').style.backgroundColor="green"
+}
+
+function change_Color_mouseOut(){
+    document.getElementById('xyz').style.backgroundColor="yellow"
+}
