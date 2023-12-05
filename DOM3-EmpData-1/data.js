48ccfa4fcd8a65403e4165e93ffd32d0f2eed0d3
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-12-05 12:51:32 +0530

    okie

---
+++ DOM3-EmpData-1/data.js
@@ -0,0 +1,8 @@
+let employees=[
+{id:101,name:'Rahul',sal:45000},
+{id:102,name:'Sonai',sal:55000},
+{id:103,name:'Priyanka',sal:65000}
+]
+function display_Data(){
+    document.getElementById('abc').innerHTML = "GM"
+}
---
+++ DOM3-EmpData/data.js
@@ -0,0 +1,23 @@
+let employees=[
+{id:101,name:'Rahul',sal:45000,gender:"Male"},
+{id:102,name:'Sonai',sal:55000,gender:"Female"},
+{id:103,name:'Priyanka',sal:65000, gender:"Female"},
+{id:104,name:'Modi Ji',sal:75000,gender:"Male"}
+
+]
+function display_Data(){
+
+    let rows=""
+    for(emp of employees){
+        if(emp.gender !="Female"){
+            rows = rows + ` <tr>
+                        <td>${emp.id}</td>
+                        <td>${emp.name}</td>
+                        <td>${emp.sal}</td>
+                        </tr> `
+        }
+        
+    }
+    document.getElementById('abc').innerHTML = rows
+    //document.getElementById('abc').innerHTML = "GM"
+}
