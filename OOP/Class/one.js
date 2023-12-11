83ab0315433074731358ee54c7b4cccac7d22fe3
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-12-11 15:34:33 +0530

    test

---
+++ OOP/Class/one.js
@@ -0,0 +1,10 @@
+class Employee{
+   eid =1001;
+   get_Details(params) {
+        console.log("GA")
+   } 
+}
+
+let e1 = new Employee()
+console.log(e1)
+e1.get_Details()
---
+++ OOP/Class/two.js
@@ -0,0 +1,11 @@
+class Account{
+    min_Bal=500
+    //+
+    open_Account(){}
+    deposit_Amount(){}
+    withdrawl(){}
+    get_Bal(){}
+    get_St(){}
+    close(){}
+}
+let a1=new Account()
